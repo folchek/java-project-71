ddd5011448d950147ba7a7a36f39214a503f0fae
Dmitrii Popov <r0sythedead@yandex.ru>
2024-09-19 17:22:42 +0300

    Added picocli, a help utility and created Makefile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,9 @@ application {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("info.picocli:picocli:4.7.6")
+    annotationProcessor ("info.picocli:picocli-codegen:4.7.6")
 }
 
 tasks.test {
