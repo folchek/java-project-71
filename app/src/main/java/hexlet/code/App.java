05c7d4a3b5d7700d0f648c438a79e2f32e9c66da
Dmitrii Popov <r0sythedead@yandex.ru>
2024-11-02 16:16:32 +0300

    commit4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,22 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
 
+import java.util.concurrent.Callable;
+
+
+@CommandLine.Command(name = "Differ",
+        mixinStandardHelpOptions = true,
+        version = "Differ 1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return null;
+    }
 }
