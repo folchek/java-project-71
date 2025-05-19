6a539640e314394d3832cb6b30ce8339ab4b4b6b
User <r0sythedead@yandex.ru>
2025-05-19 17:36:53 +0300

    4 commit

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,10 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
 
 @Command(
         name = "gendiff",
@@ -9,11 +13,35 @@ import picocli.CommandLine.Command;
         mixinStandardHelpOptions = true,
         version = "1.0"
 )
-public class App implements Runnable {
+public class App implements Callable<Integer> {
+
+    @Parameters(
+            index = "0",
+            description = "path to first file",
+            paramLabel = "filepath1"
+    )
+    private Path file1;
+
+    @Parameters(
+            index = "1",
+            description = "path to second file",
+            paramLabel = "filepath2"
+    )
+    private Path file2;
+
+    @Option(
+            names = {"-f", "--format"},
+            description = "output format [default: ${DEFAULT-VALUE}]",
+            defaultValue = "stylish",
+            paramLabel = "format"
+    )
+    private String format;
 
     @Override
-    public void run() {
-        System.out.println("Comparing files...");
+    public Integer call() {
+        System.out.printf("Comparing files:%n%s%n%s%nFormat: %s%n",
+                file1, file2, format);
+        return 0;
     }
 
     public static void main(String[] args) {
