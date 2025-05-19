5279a049e969f49f165223cde6c04a70a6da999e
User <r0sythedead@yandex.ru>
2025-05-19 16:32:08 +0300

    3 commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("info.picocli:picocli:4.7.5")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.5")
 }
 
 tasks.test {
